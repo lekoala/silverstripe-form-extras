8500fb0e4548b5a5c34ee289e12141ca7bd58db3
Thomas Portelange <thomas.portelange@boomerang.be>
2014-05-27 17:17:25 +0200

    dev

--- code/FormExtra.php
+++ code/FormExtra.php
@@ -2,29 +2,81 @@
 
 /**
  * FormExtra
+ * 
+ * Extend your forms from this class instead of Form to add extra functionalities
+ * - Support for Unvalidated actions
+ * - Shortcut for returning messages or errors
  *
  * @author lekoala
  */
 class FormExtra extends Form {
+	
+	const MSG_NOTICE = 'notice';
+	const MSG_WARNING = 'warning';
+	const MSG_BAD = 'bad';
+	const MSG_GOOD = 'good';
 
 	public function __construct($controller = null, $name = null, FieldList $fields = null, FieldList $actions = null, $validator = null) {
-		if($controller === null) {
+		if ($controller === null) {
 			$controller = Controller::curr();
 		}
-		if($name === null) {
+		if ($name === null) {
 			$name = get_called_class();
 		}
-		if($fields === null) {
+		if ($fields === null) {
 			$fields = new FieldList;
 		}
-		if($actions === null) {
+		if ($actions === null) {
 			$actions = new FieldList;
 		}
 		parent::__construct($controller, $name, $fields, $actions);
 	}
+
+	public function validate() {
+		//add support for unvalidated actions
+		$action = $this->Actions()->dataFieldByName('action_' . Form::current_action());
+		if (get_class($action) === 'FormActionUnvalidated') {
+			return true;
+		}
+		return parent::validate();
+	}
+
+	/**
+	 * Shortcut for an error
+	 * 
+	 * @param string $msg
+	 * @param string $url
+	 * @return SS_HTTPResponse
+	 */
+	protected function err($msg, $url = null) {
+		return $this->msg($msg, self::MSG_BAD, $url);
+	}
 	
-	protected function err($msg) {
-		$this->sessionMessage($msg, 'bad');
+	/**
+	 * Shortcut for a success
+	 * 
+	 * @param string $msg
+	 * @param string $url
+	 * @return SS_HTTPResponse
+	 */
+	protected function success($msg, $url = null) {
+		return $this->msg($msg, self::MSG_GOOD, $url);
+	}
+	
+	/**
+	 * Return a response with a message for your form
+	 * 
+	 * @param string $msg
+	 * @param string $type good,bad,notice,warning
+	 * @param string $url
+	 * @return SS_HTTPResponse
+	 */
+	protected function msg($msg, $type, $url = null) {
+		$this->sessionMessage($msg, $type);
+		if($url) {
+			return $this->Controller()->redirect($url);
+		}
 		return $this->Controller()->redirectBack();
 	}
+
 }
