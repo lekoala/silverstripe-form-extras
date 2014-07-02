84682018ac1291e9d127601348d6afd8c0bba190
Thomas Portelange <thomas.portelange@boomerang.be>
2014-07-02 12:40:56 +0200

    fix missing class

---
+++ code/RegexTextField.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * A text field that validates that its contents match a regular expression.
+ *
+ * @package silverstripe-addressable
+ */
+class RegexTextField extends TextField {
+
+	protected $regex;
+	
+	public function Type() {
+		return 'regextext text';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRegex() {
+		return $this->regex;
+	}
+
+	/**
+	 * @param string $regex
+	 */
+	public function setRegex($regex) {
+		$this->regex = $regex;
+	}
+
+	public function validate($validator) {
+		if($this->value && $this->regex) {
+			if(!preg_match($this->regex, $this->value)) {
+				$message = _t('RegexTextField.VALIDATE', 'Please enter a valid format for "%s".');
+				$validator->validationError($this->name, sprintf($message, $this->name), 'validation');
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+}
---
+++ javascript/SliderField.js
@@ -0,0 +1,28 @@
+/**
+ * ComboBox functionnality
+ */
+(function($) {
+	$(function() {
+
+		$('.field.slider .ui-slider').each(function() {
+			var $this = $(this);
+			var fieldHolder = $this.parents('.field');
+			var input = fieldHolder.find('input');
+			var span = fieldHolder.find('.readonly');
+			
+			var baseOptions = $this.data('options');
+			var opts = {
+				value : input.val(),
+				slide:function(event,ui) {
+					input.val(ui.value);
+					if(span.length) {
+						span.text(ui.value);
+					}
+				}
+			};
+			var options = $.extend(baseOptions,opts);
+			
+			$this.slider(options);			
+		});
+	});
+})(jQuery);
