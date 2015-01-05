d3ea213e0d19e10b7243831a7eb6e006f7e236ff
Thomas Portelange <thomas.portelange@boomerang.be>
2015-01-05 17:42:04 +0100

    make sortable optional

--- code/GridFieldConfig_Default.php
+++ code/GridFieldConfig_Default.php
@@ -7,13 +7,15 @@
  */
 class GridFieldConfig_Default extends GridFieldConfig_RelationEditor {
 
-	public function __construct($itemsPerPage=null) {
+	public function __construct($itemsPerPage=null, $sort = false) {
 		parent::__construct($itemsPerPage);
 		
 		$this->removeComponentsByType('GridFieldAddExistingAutocompleter');
 		$this->removeComponentsByType('GridFieldDeleteAction');
 		$this->addComponent(new GridFieldDeleteAction(false));
-		$this->addComponent(new GridFieldOrderableRows());
+        if($sort) {
+            $this->addComponent(new GridFieldOrderableRows());
+        }
 	}
 
 }
