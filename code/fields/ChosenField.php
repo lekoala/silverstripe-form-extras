ac83f0783e1eee519fd4e6063ca26508345ae1d0
Thomas <thomas@lekoala.be>
2015-02-13 15:57:43 +0100

    chosen field

---
+++ code/fields/ChosenField.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * A dropdown field using chosen
+ *
+ * @author Koala
+ */
+class ChosenField extends ListboxField
+{
+    protected $no_results_text;
+    protected $allow_single_deselect = true;
+    protected $allow_max_selected;
+
+    public function __construct($name, $title = null, $source = array(),
+                                $value = '', $form = null, $emptyString = null)
+    {
+        parent::__construct($name, $title, $source, $value, $form, $emptyString);
+        $this->no_results_text = _t('ChosenField.NO_RESULTS',
+            'Oops, nothing found!');
+    }
+
+    public function Field($properties = array())
+    {
+        // Use updated version of Chosen
+        Requirements::block(FRAMEWORK_ADMIN_DIR.'/thirdparty/chosen/chosen/chosen.css');
+        Requirements::block(FRAMEWORK_ADMIN_DIR.'/thirdparty/chosen/chosen/chosen.jquery.js');
+        Requirements::css(FORM_EXTRAS_PATH.'/javascript/chosen/chosen.min.css');
+        Requirements::javascript(FORM_EXTRAS_PATH.'/javascript/chosen/chosen.jquery.min.js');
+
+        // Init
+        $opts = array(
+            'no_results_text' => $this->no_results_text,
+            'allow_single_deselect' => $this->allow_single_deselect ? true : false
+        );
+        if (self::config()->rtl) {
+            $this->addExtraClass('chosen-rtl');
+        }
+        if ($this->allow_max_selected) {
+            $opts['allow_max_selected'] = $this->allow_max_selected;
+        }
+        Requirements::customScript('$("#'.$this->ID().'").chosen('.json_encode($opts).')');
+
+        return parent::Field($properties);
+    }
+
+    public function getNoResultsText()
+    {
+        return $this->no_results_text;
+    }
+
+    public function setNoResultsText($t)
+    {
+        $this->no_results_text = $t;
+    }
+
+    public function getSingleDeselect()
+    {
+        return $this->allow_single_deselect;
+    }
+
+    public function setSingleDeselect($v)
+    {
+        $this->allow_single_deselect = $v;
+    }
+
+    public function getMaxSelected()
+    {
+        return $this->allow_max_selected;
+    }
+
+    public function setMaxSelected($max)
+    {
+        $this->allow_max_selected = $max;
+    }
+
+    public function getDefaultText()
+    {
+        return $this->getAttribute('data-placeholder');
+    }
+
+    public function setDefaultText($text)
+    {
+        return $this->setAttribute('data-placeholder', $text);
+    }
+}
