16f2a96beb545f812c61a1ee477fbe3ce9024a4c
Thomas Portelange <thomas.portelange@boomerang.be>
2015-03-24 17:16:39 +0100

    table field - a simple way for managing simple array of data in a field

---
+++ code/fieldtypes/DBJsonField.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * DBJsonField
+ *
+ * A field designed to store a small json string
+ *
+ * @author lekoala
+ */
+class DBJsonField extends Varchar
+{
+
+    public function getValue()
+    {
+        return json_decode($this->value);
+    }
+
+    public function setValue($value, $record = null)
+    {
+        if(!is_string($value)) {
+            $value = json_encode($value);
+        }
+        return parent::setValue($value, $record);
+    }
+}
---
+++ code/fieldtypes/DBLargeJsonField.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * DBJsonField
+ *
+ * A field designed to store a large json string
+ *
+ * @author lekoala
+ */
+class DBJsonField extends Text
+{
+
+    public function getValue()
+    {
+        return json_decode($this->value);
+    }
+
+    public function setValue($value, $record = null)
+    {
+        if (!is_string($value)) {
+            $value = json_encode($value);
+        }
+        return parent::setValue($value, $record);
+    }
+}
