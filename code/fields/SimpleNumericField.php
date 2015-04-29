6d8245df1f12690b4a0bcb3472111a28bd4ff688
Thomas Portelange <thomas.portelange@boomerang.be>
2015-04-29 14:23:13 +0200

    drop simple numeric field in favour if inputmask

--- code/fields/SimpleNumericField.php
+++ code/fields/SimpleNumericField.php
@@ -1,99 +1,25 @@
 <?php
 
 /**
- * A numeric field that don't care about the current locale
+ * A currency field with some default behaviours
  *
  * @author Koala
  */
-class SimpleNumericField extends TextField
+class MaskedCurrencyField extends MaskedInputField
 {
 
-    public function setValue($value, $data = array())
+    public function __construct($name, $title = null, $value = '',
+                                $maxLength = null, $form = null)
     {
-        // If passing in a non-string number, or a value
-        // directly from a dataobject then localise this number
-        if ((is_numeric($value) && !is_string($value)) ||
-            ($value && $data instanceof DataObject)
-        ) {
-            $this->value = $value;
-        } else {
-            // If an invalid number, store it anyway, but validate() will fail
-            $this->value = $this->clean($value);
-        }
-        return $this;
+        parent::__construct($name, $title, $value, $maxLength, $form);
+        $this->setAlias(MaskedInputField::ALIAS_DECIMAL);
+        $this->setDigits(2);
+        $this->setRightAlign(false);
     }
 
-    /**
-     * In some cases and locales, validation expects non-breaking spaces
-     *
-     * @param string $input
-     * @return string The input value, with all spaces replaced with non-breaking spaces
-     */
-    protected function clean($input)
+    public function setValue($value)
     {
-        $nbsp = html_entity_decode('&nbsp;', null, 'UTF-8');
-        $input = str_replace(',', '.', $input);
-        return str_replace(' ', $nbsp, trim($input));
-    }
-
-    /**
-     * Determine if the current value is a valid number
-     *
-     * @return bool
-     */
-    protected function isNumeric()
-    {
-        return is_numeric($this->clean($this->value));
-    }
-
-    public function Type()
-    {
-        return 'numeric text';
-    }
-
-    public function validate($validator)
-    {
-        if (!$this->value && !$validator->fieldIsRequired($this->name)) {
-            return true;
-        }
-
-        if ($this->isNumeric()) {
-            return true;
-        }
-        
-        $validator->validationError(
-            $this->name,
-            _t(
-                'NumericField.VALIDATION',
-                "'{value}' is not a number, only numbers can be accepted for this field",
-                array('value' => $this->value)
-            ), "validation"
-        );
-        return false;
-    }
-
-    /**
-     * Extracts the number value from the localised string value
-     *
-     * @return string number value
-     */
-    public function dataValue()
-    {
-        if (!$this->isNumeric()) {
-            return 0;
-        }
-        $number = $this->clean($this->value);
-        return $number;
-    }
-
-    /**
-     * Returns a readonly version of this field
-     */
-    public function performReadonlyTransformation()
-    {
-        $field = new NumericField_Readonly($this->name, $this->title,
-            $this->value);
-        $field->setForm($this->form);
-        return $field;
+        $value = (float) $value;
+        return parent::setValue($value);
     }
 }
