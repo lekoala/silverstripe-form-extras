34ed12c0635cc7e61f990ff96055c9b17d8737bb
Thomas Saldi <thomas.saldi@gmail.com>
2015-04-29 15:40:18 +0200

    do not load false values

--- code/FormExtra.php
+++ code/FormExtra.php
@@ -53,7 +53,7 @@ class FormExtra extends Form
     {
         $data = $this->getDataFromSession();
         if($data) {
-            $this->loadDataFrom($data);
+            $this->loadDataFrom($data,Form::MERGE_IGNORE_FALSEISH);
         }
     }
 
