29618a1fce4c8b0d7f95e026a58c95939325ef4a
Thomas <thomas@lekoala.be>
2015-05-20 15:22:06 +0200

    improve masked currency field to support radix according to locale

---
+++ code/fields/MaskedCurrencyField.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * A currency field with some default behaviours
+ *
+ * @author Koala
+ */
+class MaskedCurrencyField extends MaskedInputField
+{
+
+    public function __construct($name, $title = null, $value = '',
+                                $maxLength = null, $form = null)
+    {
+        parent::__construct($name, $title, $value, $maxLength, $form);
+        $this->setAlias(MaskedInputField::ALIAS_DECIMAL);
+        $this->setDigits(2);
+        $this->setRightAlign(false);
+
+        // Some locale use "," as radix
+        $locale  = i18n::get_locale();
+        $symbols = Zend_Locale_Data::getList($locale, 'symbols');
+        if (!empty($symbols) && $symbols['decimal'] == ',') {
+            $this->setRadixPoint(',');
+        }
+    }
+
+    public function setValue($value)
+    {
+        // Just make sure that whatever value we pass in set in correct locale
+        if ($this->getRadixPoint() == '.') {
+            $value = str_replace(',', '.', $value);
+        } else {
+            $value = str_replace('.', ',', $value);
+        }
+        return parent::setValue($value);
+    }
+
+    public function dataValue()
+    {
+        // Data value should always be a valid number to be savec in the database
+        return str_replace(',', '.', parent::dataValue());
+    }
+}
