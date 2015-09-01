41673e279785a0871e93105825ebaead3b01fbf2
Thomas <thomas@lekoala.be>
2015-09-01 13:36:04 +0200

    add parsley custom validator to make use of accounting

--- javascript/AccountingField.js
+++ javascript/AccountingField.js
@@ -6,10 +6,21 @@
 		$(document).on('blur', '.field.accounting input', function () {
 			var $this = $(this);
 			var precision = 2;
-			if($this.data('precision') !== undefined) {
+			if ($this.data('precision') !== undefined) {
 				precision = $this.data('precision');
 			}
 			$this.val(accounting.formatNumber($this.val(), precision));
 		});
 	});
 })(jQuery);
+
+if (window.ParsleyValidator) {
+	window.ParsleyValidator.addValidator('accountingmin', 
+    function (value, requirement) {
+        return accounting.unformat(value) >= requirement
+    }, 32);
+	window.ParsleyValidator.addValidator('accountingmax', 
+    function (value, requirement) {
+        return accounting.unformat(value) <= requirement
+    }, 32);
+}
