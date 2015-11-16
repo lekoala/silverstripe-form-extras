55f58be7a23a2e5d36d7f180e4e09ed0c870d15b
Thomas <thomas@lekoala.be>
2015-11-16 14:58:15 +0100

    better upload

---
+++ code/fields/BetterUploadField.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Make files downloadable
+ *
+ * @author LeKoala <thomas@lekoala.be>
+ */
+class BetterUploadField extends UploadField
+{
+    public function canDownload() {
+        return Permission::check('CMS_ACCESS');
+    }
+    public function getTemplate()
+    {
+        return 'forms/BetterUploadField';
+    }
+
+}
