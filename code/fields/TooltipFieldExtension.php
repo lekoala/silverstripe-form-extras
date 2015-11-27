aedb6593f3d4bf554b320e704487475ad0ad9a33
Thomas <thomas@lekoala.be>
2015-11-27 13:38:35 +0100

    cosmetic readonly extension

--- code/fields/TooltipFieldExtension.php
+++ code/fields/TooltipFieldExtension.php
@@ -8,7 +8,6 @@
 class TooltipFieldExtension extends Extension
 {
     private static $icon = 'uk-icon-question-circle';
-
     protected $tooltip;
 
     public function getTooltip()
@@ -22,15 +21,28 @@ class TooltipFieldExtension extends Extension
 
         $t = $this->owner->Title();
 
-        if(Controller::has_curr() && Controller::curr() instanceof LeftAndMain) {
+        if (Controller::has_curr() && Controller::curr() instanceof LeftAndMain) {
             $t .= ' <span title="'.$tooltip.'" class="ui-icon ui-icon-info" style="display:inline-block;"></span>';
+        } else {
+            $t .= ' <i class="'.Config::inst()->get(__CLASS__, 'icon').' tooltip" title="'.$tooltip.'"></i>';
         }
-        else {
-            $t .= ' <i class="'.Config::inst()->get(__CLASS__,'icon').' tooltip" title="'.$tooltip.'"></i>';
-        }
-        
+
         $this->owner->setTitle($t);
 
         return $this->owner;
+    }
+
+    public function getReadonlyUI()
+    {
+        return $this->owner->getAttribute('readonly');
+    }
+
+    public function setReadonlyUI($v = true)
+    {
+        /* @var $field FormField */
+        $field = $this->owner;
+        if ($v) {
+            $field->setAttribute('readonly', 'readonly');
+        }
     }
 }
