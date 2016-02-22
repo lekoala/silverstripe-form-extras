bc6cbe38f510f0ed23b90f59399d48fd8637d20d
Thomas <thomas@lekoala.be>
2016-02-22 16:51:09 +0100

    color helpers

--- code/fieldtypes/DBColor.php
+++ code/fieldtypes/DBColor.php
@@ -16,6 +16,67 @@ class DBColor extends Varchar
         parent::__construct($name, $size, $options);
     }
 
+    public function hex6($color)
+    {
+        $color = str_replace('#', '', $color);
+        if (strlen($color) == 6) {
+            return '#'.$color;
+        }
+        if (strlen($color) == 3) {
+            return '#'.$color[0].$color[0].$color[1].$color[1].$color[2].$color[2];
+        }
+        throw new Exception("Invalid color");
+    }
+
+    /**
+     * Get inverted color
+     * 
+     * @param string $color
+     * @return string
+     */
+    public function getInvertedColor($color)
+    {
+        $color = str_replace('#', '', $this->hex6($color));
+
+        if (strlen($color) != 6) {
+            return '#000000';
+        }
+
+        $rgb = '';
+        for ($x = 0; $x < 3; $x++) {
+            $c = 255 - hexdec(substr($color, (2 * $x), 2));
+            $c = ($c < 0) ? 0 : dechex($c);
+            $rgb .= (strlen($c) < 2) ? '0'.$c : $c;
+        }
+        return '#'.$rgb;
+    }
+
+    /**
+     * Get a light or dark color based on the contrast required for a good readability
+     * 
+     * @param string $color
+     * @param string $dark
+     * @param string $light
+     * @return string
+     */
+    public function getLightOrDark($color, $dark = '#000000', $light = '#FFFFFF')
+    {
+        $color = str_replace('#', '', $this->hex6($color));
+
+        $r = hexdec(substr($color, 0, 2));
+        $g = hexdec(substr($color, 2, 2));
+        $b = hexdec(substr($color, 4, 2));
+
+        $brightness = (($r * 299) + ($g * 587) + ($b * 114)) / 1000;
+
+        return ($brightness >= 155) ? $dark : $light;
+    }
+
+    /**
+     * Get rgb value as array
+     * 
+     * @return array
+     */
     public function toRgbArray()
     {
         $hex = str_replace("#", "", $this->getValue());
@@ -33,8 +94,24 @@ class DBColor extends Varchar
         return $rgb;
     }
 
+    /**
+     * Get rgb value
+     *
+     * @return string
+     */
     public function toRgb()
     {
         return implode(",", $this->toRgbArray());
+    }
+
+    /**
+     * Get rgba value
+     * 
+     * @param float $opacity
+     * @return string
+     */
+    public function toRgba($opacity = 0.5)
+    {
+        return implode(",", $this->toRgbArray()).','.$opacity;
     }
 }
