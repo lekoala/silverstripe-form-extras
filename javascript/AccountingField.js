7a85f2be7c32d9baec27fe66d35dc4e598f74c56
Thomas <thomas@lekoala.be>
2017-02-15 16:56:52 +0100

    don't attempt to set value on disabled or readonly fields

--- javascript/AccountingField.js
+++ javascript/AccountingField.js
@@ -1,33 +1,39 @@
 /**
  * AccountingField
  */
-(function ($) {
-	$(function () {
-		$(document).on('focus', '.field.accounting input', function () {
-			var $this = $(this);
-			var options = ['0','0.00','0,00'];
-			if(options.indexOf($this.val()) > -1) {
-				$this.val('');
-			}
-		});
-		$(document).on('blur', '.field.accounting input', function () {
-			var $this = $(this);
-			var precision = 2;
-			if ($this.data('precision') !== undefined) {
-				precision = $this.data('precision');
-			}
-			$this.val(accounting.formatNumber($this.val(), precision));
-		});
-	});
+(function($) {
+    $(function() {
+        $(document).on('focus', '.field.accounting input', function() {
+            var $this = $(this);
+            var options = ['0', '0.00', '0,00'];
+            if ($this.is(':disabled') || $this.attr('readonly') == "readonly") {
+                return;
+            }
+            if (options.indexOf($this.val()) > -1) {
+                $this.val('');
+            }
+        });
+        $(document).on('blur', '.field.accounting input', function() {
+            var $this = $(this);
+            var precision = 2;
+            if ($this.is(':disabled') || $this.attr('readonly') == "readonly") {
+                return;
+            }
+            if ($this.data('precision') !== undefined) {
+                precision = $this.data('precision');
+            }
+            $this.val(accounting.formatNumber($this.val(), precision));
+        });
+    });
 })(jQuery);
 
 if (window.Parsley) {
     window.Parsley.addValidator('accountingmin',
-    function (value, requirement) {
-        return accounting.unformat(value) >= requirement
-    }, 32);
+            function(value, requirement) {
+                return accounting.unformat(value) >= requirement
+            }, 32);
     window.Parsley.addValidator('accountingmax',
-    function (value, requirement) {
-        return accounting.unformat(value) <= requirement
-    }, 32);
+            function(value, requirement) {
+                return accounting.unformat(value) <= requirement
+            }, 32);
 }
