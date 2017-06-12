7912dbf99daf3261e5b5aa8ce84a83a40036f689
Thomas <thomas@lekoala.be>
2017-06-12 15:48:07 +0200

    add integer field class

---
+++ code/fields/IntegerField.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * IntegerField
+ *
+ * @author Kalyptus SPRL <thomas@kalyptus.be>
+ */
+class IntegerField extends TextField
+{
+
+    public function extraClass()
+    {
+        return 'text ' . parent::extraClass();
+    }
+
+    public function getAttributes()
+    {
+        $attributes = [
+            'type' => 'numeric',
+            'step' => '1',
+            'oninput' => "this.value=this.value.replace(/[^0-9]/g,'');",
+            'onfocus' => "if(parseInt(this.value)==0){this.value=''}",
+            'onblur' => "if(this.value==''){this.value='0'}",
+        ];
+        return array_merge(
+            parent::getAttributes(), $attributes
+        );
+    }
+}
