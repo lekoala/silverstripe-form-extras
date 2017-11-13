5c88cfc862a29a3f25fd857c50de484d3b397ccd
Thomas <thomas@lekoala.be>
2017-11-13 17:19:54 +0100

    bootstrap columns

--- code/fields/ColumnHolderField.php
+++ code/fields/ColumnHolderField.php
@@ -10,11 +10,35 @@ class ColumnHolderField extends CompositeField
 
     protected $leftCol;
     protected $rightCol;
+    protected $widths;
 
     public function __construct($children = null)
     {
         parent::__construct($children);
         $this->addExtraClass('col-holder clearfix');
+        $this->widths = array(6, 6);
+    }
+
+    public function getWidths()
+    {
+        return $this->widths;
+    }
+
+    public function setWidths($left, $right)
+    {
+        $this->widths = array($left, $right);
+    }
+
+    public function getColClass($pos)
+    {
+        $width = $this->widths[$pos];
+        $theme = self::config()->class_theme;
+        switch ($theme) {
+            case 'bootstrap3':
+                return 'col-md-' . $width;
+            default:
+                return 'col-' . $width;
+        }
     }
 
     /**
@@ -25,6 +49,7 @@ class ColumnHolderField extends CompositeField
     {
         if (!$this->leftCol) {
             $this->leftCol = new ColumnField('left');
+            $this->leftCol->addExtraClass($this->getColClass(0));
             $this->push($this->leftCol);
         }
         return $this->leftCol;
@@ -38,6 +63,7 @@ class ColumnHolderField extends CompositeField
     {
         if (!$this->rightCol) {
             $this->rightCol = new ColumnField('right');
+            $this->rightCol->addExtraClass($this->getColClass(1));
             $this->push($this->rightCol);
         }
         return $this->rightCol;
