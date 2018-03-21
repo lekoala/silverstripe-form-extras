1d669d38f9f94060929dcf2f5b4a1b18639ac18f
Thomas <thomas@lekoala.be>
2018-03-21 16:11:20 +0100

    paste can lag a little

--- javascript/EmbedTextareaField.js
+++ javascript/EmbedTextareaField.js
@@ -1,46 +1,51 @@
 /**
  * EmbedTextAreaField
  */
-(function($) {
-    $(function() {
-        function attach(item) {
-        }
+(function ($) {
+    $(function () {
+        function attach(item) {}
+
         function validateURL(val) {
             var urlregex = new RegExp(
-                    "^(http:\/\/www.|https:\/\/www.|ftp:\/\/www.|www.){1}([0-9A-Za-z]+\.)");
+                "^(http:\/\/www.|https:\/\/www.|ftp:\/\/www.|www.){1}([0-9A-Za-z]+\.)");
             return urlregex.test(val);
         }
+
         function onPaste(event) {
             var target = $(event.target);
-            var val = target.val();
 
-            if (validateURL(val)) {
-                target.val('...');
-                target.attr('readonly', 'readonly');
-                $.getJSON('/__upload/oembed/?embed_url=' + encodeURI(val), function(res) {
-                    if (res.html) {
-                        target.val(res.html);
-                    }
-                    target.removeAttr('readonly');
-                }).error(function() {
-                    target.val(val);
-                    target.removeAttr('readonly');
-                });
-            }
+            setTimeout(function () {
+                var val = target.val();
+
+                if (validateURL(val)) {
+                    target.val('...');
+                    target.attr('readonly', 'readonly');
+                    $.getJSON('/__upload/oembed/?embed_url=' + encodeURI(val), function (res) {
+                        if (res.html) {
+                            target.val(res.html);
+                        }
+                        target.removeAttr('readonly');
+                    }).error(function () {
+                        target.val(val);
+                        target.removeAttr('readonly');
+                    });
+                }
+            }, 100);
         }
         if ($.entwine) {
-            $.entwine('ss', function($) {
+            $.entwine('ss', function ($) {
                 $('.field.embedtextarea textarea').entwine({
-                    onmatch: function() {
+                    onmatch: function () {
                         this._super();
                     },
-                    onpaste: onPaste
+                    onpaste: onPaste,
+                    onblur: onPaste
                 });
             });
         } else {
             // Init
-            $('.field.embedtextarea textarea').each(function() {
-                $(this).on('paste', onPaste);
+            $('.field.embedtextarea textarea').each(function () {
+                $(this).on('paste blur', onPaste);
             });
         }
     });
