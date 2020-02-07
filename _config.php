76d7b3f584761f09d679620d12e3029c52e0cb41
Thomas Portelange <thomas@lekoala.be>
2020-02-07 12:45:22 +0100

    7.2 compat

--- _config.php
+++ _config.php
@@ -1,3 +1,7 @@
 <?php
 
-if(!defined('FORM_EXTRAS_PATH')) define('FORM_EXTRAS_PATH', rtrim(basename(dirname(__FILE__))));
+if (!defined('FORM_EXTRAS_PATH')) define('FORM_EXTRAS_PATH', rtrim(basename(dirname(__FILE__))));
+
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
